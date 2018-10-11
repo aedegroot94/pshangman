41247065782f87c98e2244dd91a4881516f795ed
aedegroot <aedegroot94@gmail.com>
2018-10-11 17:31:52 +0200

    did some imput validation on the functions that are supposed to be called by the player, and added a win state (quite important, lol).

--- Hangman.ps1
+++ Hangman.ps1
@@ -8,11 +8,27 @@ $gamestate = @{
 
 }
 
-Function Get-Word($minlength, $maxlength) {
+$config = @{
+    minletters = 4
+    maxletters = 12
+    mistakes = 5
+}
+
+Function hangmanConfig([int]$minletters = $config.minletters, [int]$maxletters = $config.maxletters, [int]$mistakes = $config.mistakes) {
+    if($minletters -gt $maxletters) {
+        $minletters=$maxletters
+    }
+    $config.minletters = $minletters
+    $config.maxletters = $maxletters
+    $config.mistakes = $mistakes
+}
+
+
+Function Get-Word() {
     $validword = ""
     while (!$validword) {
         $index = Get-Random -Minimum 0 -Maximum $($dictionary.Length - 1)
-        If($dictionary[$index].Length -ge $minlength -and $dictionary[$index].Length -le $maxlength) {
+        If($dictionary[$index].Length -ge $config.minletters -and $dictionary[$index].Length -le $config.maxletters) {
            $validword = $dictionary[$index]
         }
     }
@@ -20,8 +36,8 @@ Function Get-Word($minlength, $maxlength) {
 }
 
 
-Function New-Game($minlength = 4, $maxlength = 12) {
-    $gamestate.currentword = Get-Word $minlength $maxlength
+Function New-Game() {
+    $gamestate.currentword = Get-Word
     $gamestate.progress = ""
     $gamestate.alreadyguessed = @()
     For($i=0;$i -lt $($gamestate.currentword.length);$i++) {
@@ -62,11 +78,14 @@ Function Get-Message($guessedright) {
     }
 }
 
-Function Guess($letter) {
+Function Guess([string]$guess) {
     #if this is this first time someone Guesses, start the first game.
     if(!$gamestate.currentword) {
         New-Game
     }
+
+    #if the given string is longer than one letter, only the first one will be used
+    $letter = $guess[0]
 
     if($gamestate.alreadyGuessed.Contains($letter)) {
         $message = "You already guessed this letter! Pick another one."
@@ -81,8 +100,11 @@ Function Guess($letter) {
             $gamestate.incorrectguesses++
             
         }
-        if($gamestate.incorrectguesses -eq 5) {
-            $message = "This was your fifth incorrect guess, game over! The word was $($gamestate.currentword). Starting a new game."
+        if($gamestate.incorrectguesses -eq $config.mistakes) {
+            $message = "You made $($gamestate.incorrectguesses) incorrect guesses, game over! The word was $($gamestate.currentword). Starting a new game."
+            New-Game 
+        } elseif (!$gamestate.progress.Contains("_")) {
+            $message = "You won! The word was $($gamestate.currentword). Starting a new game."
             New-Game 
         } else {
             $message = Get-Message $guessedright
--- README.md
+++ README.md
@@ -20,3 +20,11 @@ You can at any point start a new game by calling the New-Game function:
 ```powershell
 New-Game
 ```
+
+You can change the gamerules using the hangmanConfig function. You can give it the minimum and maximum amount of letters a word can have, and the amount of mistakes until a game over. You can set one or multiple parameters at a time:
+```powershell
+hangmanConfig -minletters 5
+hangmanConfig -maxletters 10  -mistakes 3
+hangmanConfig -minletters 4 -maxletters 9 -mistakes 6
+```
+Note: If minletters is higher than maxletters, minletters will be set equal to maxletters to avoid breaking the game.
