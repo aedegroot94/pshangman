a76a7cc1d71ec519c56cab6dbe04d8f3148dbae6
aedegroot <aedegroot94@gmail.com>
2018-10-11 16:42:33 +0200

    rookie mistake... added the code to the commit this time :)

---
+++ Hangman.ps1
@@ -0,0 +1,93 @@
+﻿$dictionary = Get-Content "./dictionary.txt"
+
+$gamestate = @{
+    currentword = ""
+    progress = ""
+    incorrectguesses = 0
+    alreadyguessed = @()
+
+}
+
+Function Get-Word($minlength, $maxlength) {
+    $validword = ""
+    while (!$validword) {
+        $index = Get-Random -Minimum 0 -Maximum $($dictionary.Length - 1)
+        If($dictionary[$index].Length -ge $minlength -and $dictionary[$index].Length -le $maxlength) {
+           $validword = $dictionary[$index]
+        }
+    }
+    return $validword  
+}
+
+
+Function New-Game($minlength = 4, $maxlength = 12) {
+    $gamestate.currentword = Get-Word $minlength $maxlength
+    $gamestate.progress = ""
+    $gamestate.alreadyguessed = @()
+    For($i=0;$i -lt $($gamestate.currentword.length);$i++) {
+         $gamestate.progress += "_"
+    }
+    $gamestate.incorrectguesses = 0
+}
+
+
+Function Get-IndexesOfLetter($gamestate, $letter) {
+    $indexes = @()
+    For($i=0;$i -lt $($gamestate.currentword.length);$i++) {
+         if ($gamestate.currentword[$i] -eq $letter) {
+            $indexes += $i
+         }
+    }
+    return $indexes
+
+}
+
+Function Update-Progress($gamestate, [array]$indexes, $letter) {
+    $newstring = ""
+    For($i=0;$i -lt $($gamestate.progress.length);$i++) {
+        if($indexes.Contains($i)) {
+            $newstring += $letter
+        } else {
+            $newstring += $gamestate.progress[$i]
+        }
+    }
+    $gamestate.progress=$newstring
+}
+
+Function Get-Message($gamestate, $guessedright) {
+    if ($guessedright) {
+        return "Correct: $($gamestate.progress)"
+    } else {
+        return "False! You made $($gamestate.incorrectguesses) incorrect guesses so far. Watch out..."
+    }
+}
+
+Function Guess($letter) {
+    #if this is this first time someone Guesses, start the first game.
+    if(!$gamestate.currentword) {
+        New-Game
+    }
+
+    if($gamestate.alreadyGuessed.Contains($letter)) {
+        $message = "You already guessed this letter! Pick another one."
+    } else {
+        $gamestate.alreadyGuessed += $letter
+        $indexes = Get-IndexesOfLetter $gamestate $letter
+        if($indexes.Length -ge 1) {
+            Update-Progress $gamestate $indexes $letter
+            $guessedright = $true
+        } else {
+            $guessedright = $false
+            $gamestate.incorrectguesses++
+            
+        }
+        if($gamestate.incorrectguesses -eq 5) {
+            $message = "This was your fifth incorrect guess, game over! The word was $($gamestate.currentword). Starting a new game."
+            New-Game 
+        } else {
+            $message = Get-Message $gamestate $guessedright
+        }
+    }  
+    return $message
+}
+
